440b27a4f81a09b3995e20b338b19bd8c8c5f365
Stefan Böhringer <github@s-boehringer.org>
2019-11-08 16:16:43 +0100

    Commit for onward development of version 0.1-0

--- R/package.R
+++ R/package.R
@@ -6,6 +6,6 @@
 #' This package simplifies package generation by automating the use of `devtools` and `roxygen`. It also makes the development workflow more efficient by allowing ad-hoc development of packages.
 #' This package allows you to create a fully-fledged R-package from a single R-file reducing the added work-load for maintaining a package to a minimum. Depending on the project, collections of files can be used and configuration can be seperated into a stand-alone configuration file, providing full flexibility. It can also handle git interaction.
 #'
-#' @seealso [createPackage()] for starting the main workflow
+#' @seealso {createPackage()} for starting the main workflow
 
 "_PACKAGE"
