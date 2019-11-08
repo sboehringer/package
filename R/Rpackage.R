27e03caa9dfb3b78f67938681e18ea0b06ed6663
Stefan Böhringer <github@s-boehringer.org>
2019-11-08 14:48:39 +0100

    Commit for onward development of version 0.1-0

---
+++ R/Rpackage.R
@@ -0,0 +1,166 @@
+#
+#	Rpackage.R
+#Thu Oct 17 16:52:03 CEST 2019
+
+packageDefinition = list(
+	name = 'package',
+	description = list(
+		title = 'Create packages from R-code directly',
+		# version to be documented in news section
+		#version = '0.1-0',
+		author = 'Stefan Böhringer <r-packages@s-boehringer.org>',
+		description = 'This package simplifies package generation by automating the use of `devtools` and `roxygen`. It also makes the development workflow more efficient by allowing ad-hoc development of packages.',
+		depends = c('roxygen2', 'devtools'),
+		suggests = c('jsonlite', 'yaml'),
+		news = "0.1-0	Initial release"
+	),
+	git = list(
+		readme = '## Installation\n```{r}\nlibrary(devtools);\ninstall_github("sboehringer/package")\n```\n',
+		push = F
+	)
+);
+
+#__PACKAGE_DOC__
+#' This is package `package`.
+#__PACKAGE_DOC_END__
+
+packageReadmeTemplate = "# R-package `%{PACKAGE_NAME}s`, version %{VERSION}s\n%{README}s\n# Description\n%{DESCRIPTION}s";
+
+packageDescTemplate = "Package: %{PACKAGE_NAME}s\nType: %{TYPE}s\nTitle: %{TITLE}s\nVersion: %{VERSION}s\nDate: %{DATE}s\nAuthor: %{AUTHOR}s\nMaintainer: %{MAINTAINER}s\nDescription: %{DESCRIPTION}s\nLicense: %{LICENSE}s\nEncoding: %{ENCODING}s\nDepends: %{DEPENDS}s\nCollate: %{COLLATE}s\nSuggests: %{SUGGESTS}s\n";
+
+packageInterpolationDict = function(o, debug = F) {
+	if (debug) cat(templ);
+	d = o$description;
+	vars = list(
+		PACKAGE_NAME = o$name,
+		TYPE = firstDef(d$type, 'Package'),
+		TITLE = d$title,
+		VERSION = firstDef(d$version, Regexpr('\\S+', d$news)[[1]], '0.1-0'),
+		DATE = firstDef(d$date, format(Sys.time(), "%d-%m-%Y")),
+		AUTHOR = firstDef(d$author, 'anonymous'),
+		MAINTAINER = firstDef(d$maintainer, d$author, 'anonymous'),
+		DESCRIPTION = firstDef(d$description, ''),
+		LICENSE = firstDef(d$license, 'LGPL'),
+		ENCODING = firstDef(d$encoding, 'UTF-8'),
+		COLLATE = circumfix(join(o$files, "\n    "), pre = "\n    "),
+		DEPENDS = circumfix(join(d$depends, ",\n    "), pre = "\n    "),
+		SUGGESTS = circumfix(join(d$suggests, ",\n    "), pre = "\n    "),
+		README = firstDef(o$git$readme, '')
+	);
+	if (debug) print(vars);
+	return(vars);
+}
+packageInterpolateVars = function(o, templ, debug = F) {
+	if (debug) cat(templ);
+	vars = packageInterpolationDict(o, debug);
+	return(with(vars, Sprintf(templ)));
+}
+packageDescription = function(o, debug = F) {
+	templ = firstDef(o$descriptionTemplate, packageDescTemplate);
+	return(packageInterpolateVars(o, templ, debug));
+}
+
+gitActions = function(o, packagesDir, debug) {
+	i = packageInterpolationDict(o, debug);
+	dir = with(o, Sprintf('%{packagesDir}s/%{name}s'));
+
+	readme = packageInterpolateVars(o, firstDef(o$git$readmeTemplate, packageReadmeTemplate));
+	writeFile(Sprintf('%{dir}s/README.md'), readme);
+	# initialize git
+	if (!file.exists(with(o, Sprintf('%{packagesDir}s/%{name}s/.git')))) {
+		System(Sprintf('cd %{dir}q ; git init'), 2);
+	}
+	System(Sprintf('cd %{dir}q ; git add --all'), 2);
+	System(with(i, Sprintf('cd %{dir}q ; git commit -a -m "Commit for onward development of version %{VERSION}s"')), 2);
+	tags = System(Sprintf('cd %{dir}q ; git tag'), 2, return.output = T)$output;
+	# tag new version
+	if (length(Regexpr(with(i, Sprintf('\\Q%{VERSION}s\\E')), tags)[[1]]) == 0) {
+		System(with(i, Sprintf('cd %{dir}q ; git tag %{VERSION}s')));
+	}
+}
+
+createPackageWithConfig = function(o, packagesDir = '~/src/Rpackages', doInstall = FALSE, debug = F) {
+	if (debug) print(o);
+
+	i = packageInterpolationDict(o, debug);
+	pdir = Sprintf('%{packagesDir}s/%{name}s', o);
+	packageDir = Sprintf('%{packagesDir}s/%{name}s', o);
+	# <p> folders
+	Dir.create(Sprintf('%{pdir}s'));
+	Dir.create(Sprintf('%{pdir}s/R'));
+
+	# <p> copy R files
+	#src = Sprintf('%{dir}s/%{files}s', dir = o$dir, files = o$files, sprintf_cartesian = T);
+	src = Sprintf('%{dir}s/%{files}s', o, sprintf_cartesian = T);
+	dest = Sprintf('%{packageDir}s/R/');
+	LogS(2, 'Copying files: %{f}s -> %{dest}s', f = join(src, ', '));
+	File.copy(src, dest, symbolicLinkIfLocal = F, overwrite = T);
+	# <p> extract package documentation
+	doc = sapply(src, function(f)
+		Regexpr('(?s)(?<=#__PACKAGE_DOC__\\n).*?(?=#__PACKAGE_DOC_END__\\n)', readFile(f))
+	);
+	# conflicts
+	Ndoc = length(which(sapply(doc, nchar) > 0));
+	if (Ndoc > 1) stop('More than one pacakge documentation found');
+	if (Ndoc == 1) {
+		if (any(sapply(src, function(f)splitPath(f)$file) == Sprintf('%{name}s.R', o)))
+			stop(Sprintf('%{name}s.R should contain package documentation which is also given elsewhere. %{name}s.R will be overwritten.', o));
+		writeFile(Sprintf('%{pdir}s/R/%{name}s.R', o), join(doc, ''));
+	}
+	# <p> update NEWS file
+	writeFile(with(o, Sprintf('%{pdir}s/NEWS')), firstDef(o$news, '0.1-0	Initial release'));
+	writeFile(with(o, Sprintf('%{pdir}s/DESCRIPTION')), packageDescription(o));
+
+	# <p> roxigen2
+	Library('devtools');
+	document(packageDir);
+
+	# <p> git
+	if (notE(o$git)) gitActions(o, packagesDir, debug);
+
+	# <p> install
+	if (doInstall) Install_local(Sprintf('%{packageDir}s'), upgrade = 'never');
+
+	return(o);
+}
+
+probeDefinition = function(desc, dir = NULL) {
+	path = Sprintf('%{pre}s%{file}s', pre = circumfix(dir, '/'), file = desc[1]);
+	sp = splitPath(path);
+	o = switch(sp$ext,
+		R = ({ Source(desc); def = get('packageDefinition'); def$files = desc; def }),
+		plist = propertyFromStringExt(readFile(path)),
+		json = ({ Library('jsonlite'); read_json(path) }),
+		yaml = ({ Library('yaml'); read_yaml(path) })
+	);
+	o$dir = firstDef(dir, sp$dir);
+	return(o);
+}
+
+#' Create package from vector of source files and single configuration
+#' 
+#' This function creates a package dir, runs documentation generation using roxygen and optionally installs the package. It can also update via git and manage version numbers. In a minimal configuration, a single file is sufficient to create a fully documented R package.
+#' 
+#' @alias createPackageWithConfig
+#' @param packageDesc path to the configuration file (R, extended plist format)
+#' @param packageDir folder in which the folder structure of the package is written
+#' @param doInstall flag to indicate whether the package should also be installed
+#' @section This function creates a valid R package folder with DESCRIPTION, LICENSE and NEWS files.
+#'	All R-files listed are copied to this directory and documentation is created by  
+#'	running the \code{devtools::document} function on this folder. details: \itemize{
+#' \item Be prepared, as in: \code{f0 =
+#' f1 = function(){42}; parallelize(f0); parallelize(f1);}
+#' \item Be extra prepared
+#' }
+#' @author Stefan Böhringer, \email{r-packages@@s-boehringer.org}
+#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
+#' @keywords ~kwd1 ~kwd2
+#' @examples
+#' 
+#' myLittlePony()
+#'
+#' @export createPackage
+createPackage = function(packageDesc, packagesDir = '~/src/Rpackages', dir = NULL, doInstall = FALSE) {
+	packageDef = probeDefinition(packageDesc, dir);
+	return(createPackageWithConfig(packageDef, packagesDir, doInstall));
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# R-package `package`, version 0.1-0
+## Installation
+```{r}
+library(devtools);
+install_github("sboehringer/package")
+```
+
+# Description
+This package simplifies package generation by automating the use of `devtools` and `roxygen`. It also makes the development workflow more efficient by allowing ad-hoc development of packages.
