1374a0d767312d2ed081e69f8d308fce67ec5ef9
Stefan Böhringer <github@s-boehringer.org>
2019-11-08 15:07:06 +0100

    Commit for onward development of version 0.1-0

--- R/Rpackage.R
+++ R/Rpackage.R
@@ -26,7 +26,7 @@ packageDefinition = list(
 #' %{title}s
 #'
 #' @details
-#' %{desciption}s
+#' %{description}s
 #'
 #' This package allows you to create a fully-fledged R-package from a single R-file reducing the added work-load for maintaining a package to a minimum. Depending on the project, collections of files can be used and configuration can be seperated into a stand-alone cofiguration file, providing full flexibility.
 #__PACKAGE_DOC_END__
--- R/package.R
+++ R/package.R
@@ -3,6 +3,6 @@
 #' Create packages from R-code directly
 #'
 #' @details
-#' NA
+#' This package simplifies package generation by automating the use of `devtools` and `roxygen`. It also makes the development workflow more efficient by allowing ad-hoc development of packages.
 #'
 #' This package allows you to create a fully-fledged R-package from a single R-file reducing the added work-load for maintaining a package to a minimum. Depending on the project, collections of files can be used and configuration can be seperated into a stand-alone cofiguration file, providing full flexibility.
