b849e738072e8a3a3531f8f515bf91e638594c43
Stefan Böhringer <github@s-boehringer.org>
2019-11-22 17:44:54 +0100

    Commit for onward development of version 0.3-1

--- inst/Rscripts/pkg-minimal.R
+++ inst/Rscripts/pkg-minimal.R
@@ -3,6 +3,28 @@
 # Fri Nov 22 17:22:55 CET 2019
 #
 
+packageDefinition = list(
+	name = 'pkg-minimal',
+	files = c(),
+	instFiles = list(),
+	description = list(
+		title = 'Create packages from R-code directly',
+		# version to be documented in news section
+		#version = '0.1-0',
+		author = 'Stefan Böhringer <r-packages@s-boehringer.org>',
+		description = 'This appears in the package-documentaion, the markdown of the git-repository and in th e package details. This package exports a single function, \\code{myLittlePony}, which prints a surprising message.',
+		depends = c(),
+		suggests = c(),
+		license = LGPL;
+		news = "0.1-0	Initial release"
+	),
+	git = list(
+		readme = '## Installation\n```{r}\nlibrary(devtools);\ninstall_github("user/pkg-minimal")\n```\n',
+		push = F,
+		pushOnNewVersion = F
+	)
+);
+
 #' Subject yourself to a message
 #' 
 #' This function will subject yourself to a message [no escape]
