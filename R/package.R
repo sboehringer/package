fd1970f2562e887a783e8345b97d28271a8f0ecf
Stefan Böhringer <github@s-boehringer.org>
2019-11-22 17:32:55 +0100

    Commit for onward development of version 0.3-1

--- R/package.R
+++ R/package.R
@@ -5,6 +5,7 @@
 #' @details
 #' This package simplifies package generation by automating the use of `devtools` and `roxygen`. It also makes the development workflow more efficient by allowing ad-hoc development of packages. Use `?"package-package"` for a tutorial.
 #' This package allows you to create a fully-fledged R-package from a single R-file reducing the added work-load for maintaining a package to a minimum. Depending on the project, collections of files can be used and configuration can be seperated into a stand-alone configuration file, providing full flexibility. It can also handle git interaction. This package is created with itself and you can look at the single R-file `Rpackage.R` for a self-referring example.
+#' The package contains a self-contained package `pkg-minimal.R` which can be inspected with `file.show(system.file('Rscripts/pkg-minimal.R', package = 'package'))`.
 #'
 #' @seealso {createPackage()} for starting the main workflow
 
