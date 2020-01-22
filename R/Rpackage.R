ab3c4b58871c5a3d8c9685904176921233585af6
Stefan Böhringer <github@s-boehringer.org>
2020-01-22 10:25:31 +0100

    Commit for onward development of version 0.5-1

--- R/Rpackage.R
+++ R/Rpackage.R
@@ -14,7 +14,8 @@ packageDefinition = list(
 		description = 'This package simplifies package generation by automating the use of `devtools` and `roxygen`. It also makes the development workflow more efficient by allowing ad-hoc development of packages. Use `?"package-package"` for a tutorial.',
 		depends = c('roxygen2', 'devtools', 'methods'),
 		suggests = c('testme', 'jsonlite', 'yaml'),
-		news = "0.5-1	Resolved documentation\n0.5-0	Error free CRAN check. Warnings left.\n0.4-4	bugfix NAMESPACE generation\n0.4-3	`createPackage` fully documented.\n0.4-2	More documentation\n0.4-1	Bug fix NEWS file\n0.4-0	Self-contained example\n0.3-1	bug fix for missing files\n0.3-0	Beta, self-contained\n0.2-0	Alpha version\n0.1-0	Initial release"
+		news = "0.5-1	Resolved documentation\n0.5-0	Error free CRAN check. Warnings left.\n0.4-4	bugfix NAMESPACE generation\n0.4-3	`createPackage` fully documented.\n0.4-2	More documentation\n0.4-1	Bug fix NEWS file\n0.4-0	Self-contained example\n0.3-1	bug fix for missing files\n0.3-0	Beta, self-contained\n0.2-0	Alpha version\n0.1-0	Initial release",
+		license = 'LGPL-2'
 	),
 	git = list(
 		readme = '## Installation\n```{r}\nlibrary(devtools);\ninstall_github("sboehringer/package")\n```\n',
@@ -59,7 +60,7 @@ packageInterpolationDict = function(o, debug = F) {
 		AUTHOR = firstDef(d$author, 'anonymous'),
 		MAINTAINER = firstDef(d$maintainer, d$author, 'anonymous'),
 		DESCRIPTION = firstDef(d$description, ''),
-		LICENSE = firstDef(d$license, 'LGPL'),
+		LICENSE = firstDef(d$license, 'LGPL-2'),
 		ENCODING = firstDef(d$encoding, 'UTF-8'),
 		COLLATE = circumfix(join(sapply(o$files, function(f)splitPath(f)$file), "\n    "), pre = "\n    "),
 		DEPENDS = circumfix(join(d$depends, ",\n    "), pre = "\n    "),
