e7f6b4344663b695aae22152497ded9bf022bef1
Stefan Böhringer <github@s-boehringer.org>
2020-01-24 11:29:22 +0100

    Commit for onward development of version 0.6-0

--- R/Rpackage.R
+++ R/Rpackage.R
@@ -134,8 +134,8 @@ installVignettes = function(o, packageDir) {
 	lapply(o$description$vignettes, function(path) {
 		v = readFile(path);
 		cat(v);
-		m = Regexpr("(?s)---\\n(?:(?<key>[a-z]\\s*):\\s*(?<value>[^\\n]*\n\\S+)\\n)+---\\n", v, captures = T);
-		#m = Regexpr("(?s)---(?:(?<key>[a-z]\\s*):\\s*)", v);
+		#m = Regexpr("(?s)---\\n(?:(?<key>[a-z]\\s*):\\s*(?<value>[^\\n]*\n\\S+)\\n)+---\\n", v, captures = T);
+		m = Regexpr("(?s)---\\n(?:(?<key>[a-z]\\s*):)", v, captures = T);
 		print(m);
 	})
 }
