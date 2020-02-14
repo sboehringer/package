f09050c8c60bdb88499150dabdbe69c7eb566ce6
Stefan Böhringer <github@s-boehringer.org>
2020-02-14 12:43:03 +0100

    Commit for onward development of version 0.8-1

--- tests/package.R
+++ tests/package.R
@@ -8,7 +8,9 @@ integrative_test = function() {
 	tmp = tempdir();
 	libPath = package:::Sprintf('%{tmp}s/lib');
 	dir.create(libPath, recursive = TRUE, showWarnings = FALSE);
-	capture.output(createPackage(system.file('Rscripts/pkg-minimal.R', package = 'package'), lib = libPath));
+	capture.output(
+		createPackage(system.file('Rscripts/pkg-minimal.R', package = 'package', noGit = T, lib = libPath))
+	);
 	roxygen2::load_installed(package:::Sprintf('%{tmp}s/pkg.minimal'));
 
 	T1 = capture.output(myLittlePony());
